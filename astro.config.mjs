a80684cafef7b5d04e5533e9186d1c1c4afb57b4
Olivier Lange <olange@users.noreply.github.com>
2023-11-08 22:07:27 +0100

    Initial commit with Astro, Shoelace and Lit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+import vercel from '@astrojs/vercel/static';
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
+  integrations: [lit()],
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'node_modules/@shoelace-style/shoelace/dist/assets/**/*',
+            dest: './shoelace/assets'
+          }
+        ]
+      })
+    ]
+  }
+});
