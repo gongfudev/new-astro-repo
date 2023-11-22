4e1b3f7f4a60ff6021452d1fe46c77cc1631d51a
Olivier Lange <olange@hey.com>
2023-11-22 21:48:08 +0100

    Removes @astrojs/lit integration for Astro
    which depends on @lit-labs/ssr and locks Lit to v2.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import vercel from '@astrojs/vercel/static';
-import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [lit()],
+  integrations: [],
   vite: {
     plugins: [
       viteStaticCopy({
